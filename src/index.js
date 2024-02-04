353d73276e29f01d875fcbe18171d2f9a9cc0aeb
kk <necessary.sone@gmail.com>
2024-02-04 16:02:10 +0900

    UglifyjsWebpackPluginでconsole.*を自動削除

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./style.css";
 import "./style.scss";
 
+console.log("I'm printing logs!")
+
 ReactDOM.render(
     <div>ハロー！</div>,
     document.getElementById('root')
